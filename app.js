2f5e4f3d28be5d7e79f38b19f51e9266e8a5a138
Sushanthsp <85091881+Sushanthsp@users.noreply.github.com>
2022-01-12 23:00:11 +0530

    jonus

--- app.js
+++ app.js
@@ -1,59 +1,12 @@
 var express = require('express')
 // var mysql = require('mysql')
-var bodyParser = require("body-parser")
+// var bodyParser = require("body-parser")
 
 var app = express()
-const port = process.env.PORT || 3000 ;
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static(__dirname + "/public"))
+app.get('/', (req,res) => {
+    res.send('Welcome to Join us app');
+})
 
-// var connection = mysql.createConnection({
-//     host : 'localhost',
-//     user : 'root',
-//     database : 'joinus',   
-//     password : 'password' 
-// });
-
-app.get('/',function(req,res){
-//    var q = 'SELECT COUNT(*) AS count FROM users';
-//    connection.query(q, function(err,results)
-//    {
-//        if(err) throw err
-//        var count = results[0].count;
-//        res.render("home" , {count : count})
-// res.send('Welecome to the joinus page')
-res.render('Welcome to the join us app')
-//    })
-}
-)
-
-app.get('/thanks',function(req,res){
-    // var q = 'SELECT COUNT(*) AS count FROM users';
-    // connection.query(q, function(err,results)
-    // {
-    //     if(err) throw err
-    //     var count = results[0].count;
-    //     res.render("thanks" , {count : count})
-    // })
-    console.log("Working")
- }
- )
-
-app.post("/register", function(req, res)
-{
-    // var person ={
-    //     email : req.body.email
-    // };
-    // connection.query('INSERT INTO users SET ?',person, function(err, result)
-    // {
-    //     if(err) throw err;
-    //     res.redirect('/thanks')
-    // })
-    console.log("Working")
-}
-)
-
-app.listen(port, () => console.log('Local app listening on port 3000!'));
-
+const port = process.env.PORT || '5000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
