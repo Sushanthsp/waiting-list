79774d14b6b41c859fcd9bf021a8ed06083f0b02
Sushanthsp <85091881+Sushanthsp@users.noreply.github.com>
2022-01-12 22:48:50 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,62 @@
-const express = require('express');
-const app = express();
+var express = require('express')
+var mysql = require('mysql')
+var bodyParser = require("body-parser")
 
-app.use(express.json());
-
+var app = express()
+const port = process.env.PORT || 3000 ;
 app.get('/', (req,res) => {
-    res.send('Welcome to app');
+    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
 })
 
 
-const port = process.env.PORT || '5000';
-app.listen(port, () => console.log(`Server started on Port ${port}`));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname + "/public"))
+
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    database : 'joinus',   
+    password : 'password' 
+});
+
+app.get('/',function(req,res){
+   var q = 'SELECT COUNT(*) AS count FROM users';
+   connection.query(q, function(err,results)
+   {
+       if(err) throw err
+       var count = results[0].count;
+       res.render("home" , {count : count})
+res.send('Welecome to the joinus page')
+   })
+}
+)
+
+app.get('/thanks',function(req,res){
+    var q = 'SELECT COUNT(*) AS count FROM users';
+    connection.query(q, function(err,results)
+    {
+        if(err) throw err
+        var count = results[0].count;
+        res.render("thanks" , {count : count})
+    })
+    console.log("Working")
+ }
+ )
+
+app.post("/register", function(req, res)
+{
+    var person ={
+        email : req.body.email
+    };
+    connection.query('INSERT INTO users SET ?',person, function(err, result)
+    {
+        if(err) throw err;
+        res.redirect('/thanks')
+    })
+    console.log("Working")
+}
+)
+
+app.listen(port, () => console.log('Local app listening on port 3000!'));
+
