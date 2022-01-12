e8a9413f93b65bf5e70f4008aa78e0f43a3d5bba
Sushanthsp <85091881+Sushanthsp@users.noreply.github.com>
2022-01-12 23:11:24 +0530

    init

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express')
-// var mysql = require('mysql')
-// var bodyParser = require("body-parser")
+var bodyParser = require("body-parser")
 
 var app = express()
-app.set('view engine', 'ejs');
 
-app.get('/', (req,res) => {
-    res.render("home");
-})
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname + "/public"))
+
+app.get('/',function(req,res){
+  
+ res.send('Welecome to the joinus page')
+
+ }
+ )
 
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
