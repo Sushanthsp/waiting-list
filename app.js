58fec9dc4d04e5e091a88739f29c83fadd4a650f
Sushanthsp <85091881+Sushanthsp@users.noreply.github.com>
2022-01-12 23:41:58 +0530

    Added thanks and redirect page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var bodyParser = require("body-parser")
 var engine = require('ejs-locals')
+var mysql = require('mysql')
 
 var app = express()
 
@@ -10,14 +11,24 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname + "/public"))
 
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    database : 'joinus',   
+    password : 'password' 
+});
 
+app.get("/",(req,res)=>{
+    res.render('home')
+    })
 
-app.get('/',function(req,res){
-  
- res.render('home')
-
- }
- )
+app.get("/thanks",(req,res)=>
+    {
+        res.render('thanks')
+    })
+app.post("/register",(req,res)=>{
+    res.redirect("/")
+    })
 
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
