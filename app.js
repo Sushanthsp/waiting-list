b04eeb6cb0133eee70b744288889c8dfb721ebbb
Sushanthsp <85091881+Sushanthsp@users.noreply.github.com>
2022-01-12 22:55:27 +0530

    init

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var mysql = require('mysql')
+// var mysql = require('mysql')
 var bodyParser = require("body-parser")
 
 var app = express()
@@ -9,47 +9,48 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname + "/public"))
 
-var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    database : 'joinus',   
-    password : 'password' 
-});
+// var connection = mysql.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     database : 'joinus',   
+//     password : 'password' 
+// });
 
 app.get('/',function(req,res){
-   var q = 'SELECT COUNT(*) AS count FROM users';
-   connection.query(q, function(err,results)
-   {
-       if(err) throw err
-       var count = results[0].count;
-       res.render("home" , {count : count})
-res.send('Welecome to the joinus page')
-   })
+//    var q = 'SELECT COUNT(*) AS count FROM users';
+//    connection.query(q, function(err,results)
+//    {
+//        if(err) throw err
+//        var count = results[0].count;
+//        res.render("home" , {count : count})
+// res.send('Welecome to the joinus page')
+res.render('Welcome to the join us app')
+//    })
 }
 )
 
 app.get('/thanks',function(req,res){
-    var q = 'SELECT COUNT(*) AS count FROM users';
-    connection.query(q, function(err,results)
-    {
-        if(err) throw err
-        var count = results[0].count;
-        res.render("thanks" , {count : count})
-    })
+    // var q = 'SELECT COUNT(*) AS count FROM users';
+    // connection.query(q, function(err,results)
+    // {
+    //     if(err) throw err
+    //     var count = results[0].count;
+    //     res.render("thanks" , {count : count})
+    // })
     console.log("Working")
  }
  )
 
 app.post("/register", function(req, res)
 {
-    var person ={
-        email : req.body.email
-    };
-    connection.query('INSERT INTO users SET ?',person, function(err, result)
-    {
-        if(err) throw err;
-        res.redirect('/thanks')
-    })
+    // var person ={
+    //     email : req.body.email
+    // };
+    // connection.query('INSERT INTO users SET ?',person, function(err, result)
+    // {
+    //     if(err) throw err;
+    //     res.redirect('/thanks')
+    // })
     console.log("Working")
 }
 )
